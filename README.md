e26992476cb3a45c0510d32e5ef2f14a8aa5fc19
Thomas TKO <hillus96@googlemail.com>
2025-06-27 21:45:01 +0200

    Add files via upload

---
+++ README.md
@@ -0,0 +1,4 @@
+# Exile-Altis-Tanoa-Malden-64Bit
+All what you need to run 64Bit Server
+With VEMFr,DMS,R3F_logistik,Statusbar,Fast Time,KillMessage,MWarning,Chop Tree on vehicle,PayCheck,Sell Box on Wastedump,purchaseVehicle on helipad,EBM and CUP on trader
+
---
+++ serverupdate.bat
@@ -0,0 +1,7 @@
+@echo off
+echo This Will Install Stable Arma3
+steamcmd +login "steam name" "password" +force_install_dir C:\Server\Exile\ +app_update 233780 validate +quit
+echo ----------------------------------------
+echo Download Compleate
+echo ----------------------------------------
+pause
